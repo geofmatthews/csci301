847bf53aba0ee4ac27b8d7cd91732f5fa87c1600
Geoffrey Matthews <geoffrey.matthews@wwu.edu>
2018-11-16 07:43:52 -0800

    ll parsing notes

---
+++ lectures/050llparsing/recursivedescent.rkt
@@ -0,0 +1,72 @@
+#lang racket
+(define *input* '(a a a b c c c $))
+(define *error* #f)
+
+(define (S1)
+  (cond 
+    ((front? 'a)
+     (displayln "S -> aSc")
+     (match 'a)
+     (S1)
+     (match 'c))
+    ((front? 'b)
+     (displayln "S -> b")
+     (match 'b))
+    (else
+     (error))))
+
+(define (error) (set! *error* #t))
+(define (front? letter) (and (not (null? *input*))
+                             (equal? (car *input*) letter)))
+(define (match letter)
+  (cond ((front? letter)
+         (set! *input* (cdr *input*)))
+        (else
+         (error))))
+(begin
+  (S1)
+  (when (not (front? '$)) (error))
+  (if *error*
+      (displayln "Error!")
+      (displayln "Success!")))
+
+(set! *input* '(a a c c b b $))
+(set! *error* #f)
+
+(define (S2)
+  (cond ((front? 'a)
+         (displayln "S -> A S b")
+         (A)
+         (S2)
+         (match 'b))
+        ((front? 'c)
+         (displayln "S -> C")
+         (C))
+        ((front? '$)
+         (displayln "S -> C")
+         (C))
+        (else (error))))
+(define (A)
+  (cond ((front? 'a)
+         (displayln "A -> a")
+         (match 'a))
+        (else (error))))
+(define (C)
+  (cond ((front? 'b)
+         (displayln "C -> empty")
+         )
+        ((front? 'c)
+         (displayln "C -> c C")
+         (match 'c)
+         (C))
+        ((front? '$)
+         (displayln "C -> empty")
+         )
+        (else (error))))
+
+(begin
+  (S2)
+  (when (not (front? '$)) (error))
+  (if *error*
+      (displayln "Error!")
+      (displayln "Success!")))
